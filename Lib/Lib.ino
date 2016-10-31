53a662a81e0ed7119aa68fb1ef143f64c53eb448
Nick Berezansky <nikob5@hotmail.com>
2016-10-31 11:28:23 -0700

    Empty Visual Studio Template

---
+++ Lib/Lib.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		Lib.ino
+ Created:	10/31/2016 11:21:46 AM
+ Author:	nikob
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "LibLib.h"
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Lib/src/_micro-api/libraries/LibLib/src/LibLib.h
@@ -0,0 +1,19 @@
+/*
+ Name:		LibLib.h
+ Created:	10/31/2016 11:21:46 AM
+ Author:	nikob
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _LibLib_h
+#define _LibLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
