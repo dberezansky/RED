9244ea1ea070d8605c5fe4d81f14a9631d62cae3
Nick Berezansky <nikob5@hotmail.com>
2016-11-04 14:51:01 -0700

    Sensor code

--- iRobot/iRobot/Sensor.h
+++ iRobot/iRobot/Sensor.h
@@ -1,12 +1,49 @@
 #include "arduino.h"
 
+// Ultrasonic sensor HC-SR04
 class Sensor
 {
 
- public:
-	 Sensor(int a, int b)
-	 {
+public:
+	void attach(int trigPin, int echoPin)
+	{
+		_trigPin = trigPin;
+		_echoPin = echoPin;
 
-	 }
+		pinMode(trigPin, OUTPUT);
+		pinMode(_echoPin, INPUT);
+
+		digitalWrite(trigPin, LOW); 
+		delay(2); 
+	}
+
+	float getDistance()
+	{
+		digitalWrite(_trigPin, HIGH); 
+		delayMicroseconds(15); 
+		digitalWrite(_trigPin, LOW);
+		delayMicroseconds(10); 
+
+		float pingTime = pulseIn(_echoPin, HIGH);
+		return mcSec2in(pingTime);
+	}
+
+private:
+	int _trigPin;
+	int _echoPin;
+
+	float speedOfSound = 776.5; // ml/h
+
+	float mcSec2in(float pingTime)
+	{
+		pingTime = pingTime / 1000000;				//pingTime is in microseconds. Convert to seconds
+		pingTime = pingTime / 3600;					//seconds to hours
+
+		float targetDistance = speedOfSound * pingTime;   //distance in miles
+		targetDistance = targetDistance / 2;			  //distance covered twice
+		targetDistance = targetDistance * 63360;		  //distance in inches (63360 inches per mile)
+
+		return targetDistance;
+	}
 };
 
--- iRobot/iRobot/iRobot.ino
+++ iRobot/iRobot/iRobot.ino
@@ -3,18 +3,22 @@
  Created:	10/31/2016 11:20:48 AM
  Author:	bcakid
 */
-
-
-
-// the setup function runs once when you press reset or power the board
 #include "Sensor.h"
 
-void setup() {
+
+// Gadgets
+Sensor s;
+
+void setup() 
+{
+	Serial.begin(9600);
+
+	s.attach(11, 13);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-
-	Sensor s(1, 1);
-   
+void loop() 
+{
+	float distance = s.getDistance();
+	Serial.println(distance);
+	delay(100);
 }
